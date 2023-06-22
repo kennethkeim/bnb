caa26a88d0889f431f90411d8d006d41e9f87d04
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-06-21 23:32:14 -0400

    Add igms auth provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,12 +1,15 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import axios from "axios";
 import { type GetServerSidePropsContext } from "next";
 import {
   getServerSession,
   type NextAuthOptions,
   type DefaultSession,
+  TokenSet,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import { env } from "~/env.mjs";
+import { IgmsHostResponse } from "~/models/igms/host.model";
 import { prisma } from "~/server/db";
 
 /**
@@ -30,6 +33,9 @@ declare module "next-auth" {
   // }
 }
 
+const igmsDomain = "https://igms.com";
+const igmsId = "igms";
+
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
  *
@@ -51,6 +57,70 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
+
+    {
+      id: igmsId,
+      name: "IGMS",
+      type: "oauth",
+      clientId: env.IGMS_CLIENT_ID,
+      clientSecret: env.IGMS_CLIENT_SECRET,
+      // checks: ["state"],
+
+      // configure call to authorize user
+      authorization: {
+        params: {
+          scope: "listings,direct-bookings,calendar-control,messaging,tasks",
+          client_id: "229",
+          redirect_uri: `http://localhost:3000/api/auth/callback/${igmsId}`,
+        },
+        url: `${igmsDomain}/app/auth.html`,
+      },
+
+      // make call to get token
+      token: {
+        async request(context) {
+          const params = {
+            grant_type: "authorization_code",
+            code: context.params.code,
+            redirect_uri: context.provider.callbackUrl,
+            client_id: env.IGMS_CLIENT_ID,
+            client_secret: env.IGMS_CLIENT_SECRET,
+          };
+          const response = await axios.get(`${igmsDomain}/auth/token`, {
+            params,
+          });
+          return { tokens: response.data as TokenSet };
+        },
+      },
+
+      // make call to get user info
+      userinfo: {
+        async request(context) {
+          const url = `${igmsDomain}/api/v1/hosts`;
+          const params = {
+            access_token: context.tokens.access_token,
+          };
+          const response = await axios.get(url, { params });
+          return response.data;
+        },
+      },
+
+      // extract user info
+      profile(response: IgmsHostResponse) {
+        console.log("GOT PROFIlE", response);
+        const user = response.data.find(
+          (host) => host.platform_type === "airbnb"
+        );
+        if (!user) throw new Error("User not found");
+        return {
+          id: user.host_uid,
+          name: user.name,
+          email: user.email[0],
+          image: user.thumbnail_url,
+        };
+      },
+    },
+
     /**
      * ...add more providers here.
      *
