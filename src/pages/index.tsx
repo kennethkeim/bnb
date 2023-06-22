48b911483df63bc9403d493419501b67ba8ba535
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-06-21 23:32:24 -0400

    Display user photo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,7 +62,7 @@ const AuthShowcase: React.FC = () => {
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined },
+    { enabled: sessionData?.user !== undefined }
   );
 
   return (
@@ -71,6 +71,11 @@ const AuthShowcase: React.FC = () => {
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
       </p>
+      <div>
+        {sessionData?.user.image && (
+          <img src={sessionData.user.image} className="w-40 rounded-full"></img>
+        )}
+      </div>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
